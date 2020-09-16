af8d5b6de7635b35f53a50c3ba6560c3b4e9bdd9
AnnaBuller <aniabela@wp.pl>
2020-09-16 13:42:00 +0200

    5th change

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-console.log('this is the major change')
 console.log('this is the 1st change')
 console.log('this is the 2nd change')
-console.log('this is the 3rd change')
+console.log('this is the 5th change')
 
 
